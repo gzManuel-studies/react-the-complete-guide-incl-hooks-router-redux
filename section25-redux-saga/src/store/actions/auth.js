13a2764596678b22ddeaff0f12d5f318a51fdccb
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-03-16 12:31:33 -0400

    413, 414, 415, 416, 417, 418 steps

--- section25-redux-saga/src/store/actions/auth.js
+++ section25-redux-saga/src/store/actions/auth.js
@@ -25,51 +25,39 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userId');
 
     return {
-        type: actionTypes.AUTH_LOGOUT,
+        type: actionTypes.AUTH_INITIATE_LOGOUT,
+    };
+}
+export const logoutSucceed = () => {
+    return {
+        type:actionTypes.AUTH_LOGOUT
     };
 }
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, (expirationTime * 1000))
-    };
+    // return dispatch => {
+    //     setTimeout(() => {
+    //         dispatch(logout());
+    //     }, (expirationTime * 1000));
+    // };
+    return {
+        type:actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime:expirationTime
+    }
 }
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const apiKey = 'AIzaSyCjx2sqoyLVv_AInVtDcdbQyaQabauZ7BE';
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
-
-        if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
-        }
-
-        axios.post(url + apiKey, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId',response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(error => {
-                dispatch(authFail(error.response.data.error));
-            });
-    };
+    return {
+        type:actionTypes.AUTH_USER,
+        email:email,
+        password:password,
+        isSignup:isSignup
+    }
 };
 
 export const setAuthRedirectPath = (path) => {
