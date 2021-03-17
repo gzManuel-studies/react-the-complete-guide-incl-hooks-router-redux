c6c80d88f20c9aab8f9a9a4d13c56db97ac304a9
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-03-17 19:08:09 -0400

    431, 432, 433, 434, 435, 436 steps

--- section26-react-hooks/src/components/Ingredients/Ingredients.js
+++ section26-react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList';
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-hooks-update-75e95-default-rtdb.firebaseio.com/ingredients.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);// with [] parameter this function component just will render when userIngredient changed.
+
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-update-75e95-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [...prevIngredients, { id: responseData.name, ...ingredient }]);
+    });
+  }
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId))
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
